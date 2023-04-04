98779158ea5e2fab7d5aca522e421a9cb29b0e8d
Mohit <mohitsuradkar274@gmail.com>
2023-04-04 16:42:43 +0530

    Create Scholarship.js

---
+++ src/Components/Scholarship.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+export default function Scholarship() {
+  return (
+    <div className='scholarship'>
+      <Sidebar/>
+      Scholarship
+    </div>
+  )
+}
