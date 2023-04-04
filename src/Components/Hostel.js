7d1e42bd4dad02eaba8492921953583f19d7ff3d
Mohit <mohitsuradkar274@gmail.com>
2023-04-04 16:42:26 +0530

    Create Hostel.js

---
+++ src/Components/Hostel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+export default function Hostel() {
+  return (
+    <div className='hostel'>
+      <Sidebar/>
+      Hostel Application
+    </div>
+  )
+}
