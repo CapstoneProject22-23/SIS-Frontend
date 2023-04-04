07aceec727d2f816efc5f0f76acb3a9f60f5e01a
Mohit <mohitsuradkar274@gmail.com>
2023-04-04 16:42:49 +0530

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../CSS/Sidebar.css";
+import { Link } from "react-router-dom";
 export default function Sidebar(props) {
   const [selected, setSelected] = new useState("btn1");
   const clicked = () => {
@@ -8,62 +9,62 @@ export default function Sidebar(props) {
   };
   return (
     <div className="sidebar">
-      <button
+      <Link to="/profile"
         className={selected === "btn1" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn1"
       >
         Profile
-      </button>
-      <button
+      </Link>
+      <Link to="/attendance"
         className={selected === "btn2" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn2"
       >
         Attendence
-      </button>
-      <button
+      </Link>
+      <Link to="/bonafide"
         className={selected === "btn3" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn3"
       >
         Bonafide
-      </button>
-      <button
+      </Link>
+      <Link to="/tc"
         className={selected === "btn4" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn4"
       >
         TC application
-      </button>
-      <button
+      </Link>
+      <Link to="/scholarships"
         className={selected === "btn5" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn5"
       >
         Scholarships
-      </button>
-      <button
+      </Link>
+      <Link to="/results"
         className={selected === "btn6" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn6"
       >
         Results
-      </button>
-      <button
+      </Link>
+      <Link to="/test"
         className={selected === "btn7" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn7"
       >
         Test marks
-      </button>
-      <button
+      </Link>
+      <Link to="/hostel"
         className={selected === "btn8" ? "btn btn-selected" : "btn btn-primary"}
         onClick={clicked}
         id="btn8"
       >
         Hostel application
-      </button>
+      </Link>
     </div>
   );
 }
