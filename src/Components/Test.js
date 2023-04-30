c3f538e00c78ca4830a54857d610d2714e103b8c
Mohit <mohitsuradkar274@gmail.com>
2023-05-01 00:31:59 +0530

    Update Test.js

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,19 +1,52 @@
 import React from "react";
 import "../CSS/Test.css"
+import Popup from 'reactjs-popup';
+import Test1 from "./TestResults/Test1";
+import Test2 from "./TestResults/Test2";
+
 function Test() {
 
-  function ct1Result(params) {
-    
-  }
+  
   return (
     <div className="testmod" >
       <div className="test container">
-      <h1>Test Module</h1>
-      <p>To check the result of particular class test (Class Test 1/2) please press the below buttons :</p>
-      <button className="btn btn-outlined-secondary" onClick={ct1Result}>CT1 Result</button>
-      <button className="btn btn-outlined-secondary">CT2 Result</button>
+        <h1>Test Module</h1>
+        <p>To check the result of particular class test (Class Test 1/2) please press the below buttons :</p>
+        <Popup
+          trigger={
+            
+            <button id="ct1" className="btn btn-outlined-secondary">
+              CT1 Result
+            </button>
+            
+          }
+          modal
+          nested
+        >
+          {/* <div className="content">
+            <Login />
+          </div> */}
+          {(close) => <Test1 cmd={close} />}
+        </Popup>
+
+        <Popup
+          trigger={
+            
+            <button id="ct1" className="btn btn-outlined-secondary">
+              CT2 Result
+            </button>
+            
+          }
+          modal
+          nested
+        >
+          {/* <div className="content">
+            <Login />
+          </div> */}
+          {(close) => <Test2 cmd={close} />}
+        </Popup>
       </div>
-      
+
     </div>
   );
 }
