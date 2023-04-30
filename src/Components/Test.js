acfe6456f879024c5c00a8169cf435ae0dc3c88a
Mohit <mohitsuradkar274@gmail.com>
2023-04-30 23:02:46 +0530

    Update Test.js

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import "../CSS/Test.css"
 function Test() {
+
+  function ct1Result(params) {
+    
+  }
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="testmod" >
+      <div className="test container">
+      <h1>Test Module</h1>
+      <p>To check the result of particular class test (Class Test 1/2) please press the below buttons :</p>
+      <button className="btn btn-outlined-secondary" onClick={ct1Result}>CT1 Result</button>
+      <button className="btn btn-outlined-secondary">CT2 Result</button>
+      </div>
+      
     </div>
   );
 }
